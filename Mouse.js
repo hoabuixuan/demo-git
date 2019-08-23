efb64a90ddcec7d39d32bf8f1ea276be03567d90
Hoabx <hoahomhinhhdgg@gmail.com>
2019-08-23 14:57:47 +0700

    Change Mouse

--- Mouse.js
+++ Mouse.js
@@ -1,7 +1,12 @@
 function Mouse() {
+<<<<<<< HEAD
     var name = 'chip chip'
     document.write('name')
 
 }
 
 module.exports = Mouse;
+=======
+    document.write('chip chip')
+}
+>>>>>>> parent of 4736f63... Change modul mouse
